a71f477159c234de16b824157227ddeb1e0d4081
Matthew Maloney <mtm9051@rit.edu>
2018-02-14 08:49:33 -0500

    Add files via upload
    Basic html form for authentication and the start of a processing script.

---
+++ authenticate.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+	<title>Authenticating...</title>
+	</head>
+	<body>
+	
+		<?php
+			$myPDO = PDO('mysql:host=localhost;dbname=dbname', 'user', 'password');
+			
+			$res = $myPDO->query("SELECT FROM");
+	</body>
+
+</html>

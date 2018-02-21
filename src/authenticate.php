fd818863dafffbd9fc7e438dc3818100785b4ebe
Peter <pete800@optonline.net>
2018-02-21 08:45:26 -0500

    Forgot to change DB name

--- src/authenticate.php
+++ src/authenticate.php
@@ -28,7 +28,7 @@
 
             // TODO: We should move some of this code to a master functions file
 			try {
-				$conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+				$conn = new PDO("mysql:host=$servername;dbname=Vote", $username, $password);
 
 				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 				echo "Connected successfully"; 

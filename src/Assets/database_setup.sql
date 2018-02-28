a238b41f60a78119fbb9624e83aaadb681852dc8
Peter <pete800@optonline.net>
2018-02-28 07:57:40 -0500

    Added Candidates and Parties to SQL script

--- src/Assets/database_setup.sql
+++ src/Assets/database_setup.sql
@@ -24,3 +24,24 @@ CREATE TABLE Voted(
 );
 
 INSERT INTO Voted VALUES("1","1","0");
+
+CREATE TABLE Candidates(
+  CandidateID int AUTO_INCREMENT PRIMARY KEY,
+  FName varchar(30),
+  LName varchar(30),
+  PartyID SMALLINT,
+  VPresFName varchar(30),
+  VPresLName varchar(30),
+  YearVote varchar(30),
+  FOREIGN KEY (PartyID) REFERENCES Parties(PartyID)
+);
+
+INSERT INTO Candidates VALUES("1","Donald","Trump","1","Mike","Pence","2018");
+INSERT INTO Candidates VALUES("2","Hillary","Clinton","2","Tim","Kaine","2018");
+
+CREATE TABLE Parties(
+  PartyID SMALLINT AUTO_INCREMENT PRIMARY KEY,
+  PartyName varchar(30)
+);
+INSERT INTO Parties VALUES("1", "Republican");
+INSERT INTO Parties VALUES("2", "Democrats");

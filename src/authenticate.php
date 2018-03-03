a395ec16a955c67552958518cd5f9661a6e0a01f
Peter <pete800@optonline.net>
2018-03-03 13:44:43 -0500

    Fixing post check

--- src/authenticate.php
+++ src/authenticate.php
@@ -1,43 +1,43 @@
 <?php
+    if(!empty($_POST)) {
+        $servername = "localhost";    // can be secured by putting this informatiion in a file below root
+        $username = "root";
+        $password = "3a5bda92e6bf62dd9b8ed6a42dc7bc7380e116126916ac5c";
+        // TODO: Create a proper environment variables in the future so this is not in the code
 
-			$servername = "localhost";    // can be secured by putting this informatiion in a file below root
-			$username = "root";
-			$password = "3a5bda92e6bf62dd9b8ed6a42dc7bc7380e116126916ac5c";
-			// TODO: Create a proper environment variables in the future so this is not in the code
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $ssn = $_POST['ssn'];
+        $street = $_POST['street'];
+        $city = $_POST['city'];
+        $state = $_POST['state'];
+        $county = $_POST['county'];
 
-			$firstname = $_POST['firstname'];
-			$lastname  = $_POST['lastname'];
-			$ssn       = $_POST['ssn'];
-			$street    = $_POST['street'];
-			$city      = $_POST['city'];
-			$state     = $_POST['state'];
-			$county    = $_POST['county'];
-
-			foreach($_POST as $value) {    // securing from injection type attacks
-				if ($value != $gender && isset($value)) {  // and not gender bc gender is not a text field
-					$value = trim($value);
-					$value = strip_tags($value);
-				}
-			}
-
-			$conn = mysqli_connect("localhost","root","3a5bda92e6bf62dd9b8ed6a42dc7bc7380e116126916ac5c","vote");
-			if(!$conn)
-            {
-                printf("Cannot connect to database");
+        foreach ($_POST as $value) {    // securing from injection type attacks
+            if ($value != $gender && isset($value)) {  // and not gender bc gender is not a text field
+                $value = trim($value);
+                $value = strip_tags($value);
             }
-            echo "Connection Made \n";
-            $result = mysqli_query($conn, "SELECT * FROM Users WHERE FName='".$firstname."' AND LName='".$lastname."' AND SSN='".$ssn."' AND Street='".$street."' AND City='".$city."' AND StateCode='".$state."' AND County='".$county."';");
-            echo "After Query\n";
-			if(mysqli_num_rows($result) == 1)
-            {
-                echo "Verification Successful";
-                mysqli_close($conn);
-                header("Location: votingbooth.html"); Die();
-            }else {
-                echo "Condition Not Met\n";
-            }
+        }
+
+        $conn = mysqli_connect("localhost", "root", "3a5bda92e6bf62dd9b8ed6a42dc7bc7380e116126916ac5c", "vote");
+        if (!$conn) {
+            printf("Cannot connect to database");
+        }
+        echo "Connection Made \n";
+        $result = mysqli_query($conn, "SELECT * FROM Users WHERE FName='" . $firstname . "' AND LName='" . $lastname . "' AND SSN='" . $ssn . "' AND Street='" . $street . "' AND City='" . $city . "' AND StateCode='" . $state . "' AND County='" . $county . "';");
+        echo "After Query\n";
+        if (mysqli_num_rows($result) == 1) {
+            echo "Verification Successful";
             mysqli_close($conn);
-			?>
+            header("Location: votingbooth.html");
+            Die();
+        } else {
+            echo "Condition Not Met\n";
+        }
+        mysqli_close($conn);
+    }
+?>
 <!DOCTYPE html>
 <html>
 	<head>

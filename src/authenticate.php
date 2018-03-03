5eb6be4247990b44d1dd166808340ad74eec464f
Peter <pete800@optonline.net>
2018-03-03 14:39:20 -0500

    Still trying to track down a bug

--- src/authenticate.php
+++ src/authenticate.php
@@ -26,12 +26,13 @@
         }
         $result = mysqli_query($conn, "SELECT * FROM Users WHERE FName='" . $firstname . "' AND LName='" . $lastname . "' AND SSN='" . $ssn . "' AND Street='" . $street . "' AND City='" . $city . "' AND StateCode='" . $state . "' AND County='" . $county . "';");
         if (mysqli_num_rows($result) == 1) {
-
             session_start();
+            $result = mysqli_fetch_assoc($result);
             $_SESSION['UserID'] = $result['UserID'];
-            header("Location: votingbooth.php");
+            echo $result['UserID'];
+            //header("Location: votingbooth.php");
             mysqli_close($conn);
-            Die();
+            //Die();
         } else {
             //TODO Toss the user to the error screen here. Unable to authenticate
         }
